f24e77a7117f4cae253528dc2e351f3456a5e814
Ella <ella.n.frandsen@gmail.com>
2025-08-19 22:52:21 -0600

    started project

---
+++ Wordle Solver/main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class main {
+    public static void createGUI(){
+        JFrame window = new JFrame("Wordle Solver");
+        
+    }
+
+
+
+    public static void main(String[] args){
+
+    }
+}
